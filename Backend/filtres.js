5192d1356c776988125a03ddc38cef0f52a8961b
ShivaArmada <gab62120@gmail.com>
2024-03-14 18:31:37 +0100

    script filtre + id p associées

--- Backend/filtres.js
+++ Backend/filtres.js
@@ -1,1 +1,27 @@
 //  pour les filtres de la page index
+// 1. Filtre par catégorie
+
+// Sélectionnez tous les boutons de filtre
+const filterButtons = document.querySelectorAll('.filter');
+
+// Sélectionnez tous les éléments de la galerie
+const galleryItems = document.querySelectorAll('.gallery figure');
+
+// Ajoutez un gestionnaire d'événements de clic à chaque bouton de filtre
+filterButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        // Obtenez la catégorie du bouton cliqué
+        const category = button.querySelector('p').id;
+
+        // Cachez tous les éléments de la galerie
+        galleryItems.forEach(item => {
+            item.style.display = 'none';
+        });
+
+        // Affichez uniquement les éléments de la catégorie sélectionnée
+        const itemsToShow = document.querySelectorAll(`.gallery .${category}`);
+        itemsToShow.forEach(item => {
+            item.style.display = 'block';
+        });
+    });
+});
