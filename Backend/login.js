de062762ba0f2ea936f2fc5815a6e0154472167b
ShivaArmada <planckaertg@gmail.com>
2024-03-18 17:10:09 +0100

    login start js

--- Backend/login.js
+++ Backend/login.js
@@ -1,15 +1,38 @@
-
-
-// Cette fonction permet d'initialiser le login en appuyant sur le bouton se connecter
-function initLogin() {
-    let conect_Btn = document.querySelector(".conect_Btn")
+   // Cette fonction permet d'initialiser le login en appuyant sur le bouton se connecter
+   function initLogin() {
+    let conect_Btn = document.getElementById("input-log-submit");
     conect_Btn.addEventListener("click", () => {
-        // Quand on a cliqué sur le bouton de connexion, on se connecte
-        loginFunction();
+        
+        // URL à utiliser pour la requête Post/GET user-e-mdp
+    const url = 'http://localhost:5678/api/users/login';
+
+    // Options de la requête Post
+    const options = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json'
+        }
+
+
+    };
+
+
+
+
+      // Options de la requête GET
+      const options = {
+          method: 'GET',
+          headers: {
+              'Accept': 'application/json'
+          }
+
+
+      };
+       
     })
 }
 
+   
+   
 
-initLogin();
 
-// Cette fonction permet de se connecter
