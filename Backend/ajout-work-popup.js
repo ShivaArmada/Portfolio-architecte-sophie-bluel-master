8ca56a8495ae99ce7d6e7904eb5d80117e010da6
ShivaArmada <gab62120@gmail.com>
2024-03-21 23:35:31 +0100

    popup-new-en-travaux
    mise en place des inputs select et category de la popup ajout photo

--- Backend/ajout-work-popup.js
+++ Backend/ajout-work-popup.js
@@ -49,19 +49,50 @@ function afficherPopupNew() {
 
         let pAddPhotoNew = document.createElement("p");
         pAddPhotoNew.innerText = "+ Ajouter photo";
+        pAddPhotoNew.classList.add("p-add-photo");
         cadreDivNew.appendChild(pAddPhotoNew);
 
         let pFormatNew = document.createElement("p");
         pFormatNew.innerText = "jpg, png : 4mo max";
+        pFormatNew.classList.add("p-format");
         cadreDivNew.appendChild(pFormatNew);
 
+        let divInputZone1 = document.createElement("div");
+        divInputZone1.classList.add("input-zone");
+        
         let inputTitleNew = document.createElement("input");
         inputTitleNew.setAttribute("type", "text");
-        inputTitleNew.setAttribute("placeholder", "Titre");
-        GformNew.appendChild(inputTitleNew);
+        inputTitleNew.setAttribute("id", "title-zone-ajout"); // Utilisez "title-zone-ajout" comme ID pour éviter toute confrontation
+        inputTitleNew.classList.add("input-reaction");
+        divInputZone1.appendChild(inputTitleNew);
+        
+        let labelTitleNew = document.createElement("label");
+        labelTitleNew.setAttribute("for", "title-zone-ajout"); // Assurez-vous que l'attribut for correspond au nouvel ID de l'élément d'entrée
+        divInputZone1.insertBefore(labelTitleNew, inputTitleNew);
+        
+        GformNew.appendChild(divInputZone1);
 
+        let divInputZone2 = document.createElement("div");
+        divInputZone2.classList.add("input-zone");
+        
         let selectCategoryNew = document.createElement("select");
         selectCategoryNew.setAttribute("name", "category");
+        selectCategoryNew.setAttribute("id", "category");
+        selectCategoryNew.classList.add("input-reaction");
+        divInputZone2.appendChild(selectCategoryNew);
+        
+        let defaultOption = document.createElement("option");
+        defaultOption.textContent = ""; // Texte de l'option par défaut
+        defaultOption.setAttribute("disabled", ""); // Désactive l'option par défaut
+        defaultOption.setAttribute("selected", ""); // Sélectionne l'option par défaut
+        selectCategoryNew.appendChild(defaultOption); // Ajoute l'option par défaut au select
+        
+        let labelCategoryNew = document.createElement("label");
+        labelCategoryNew.setAttribute("for", "category");
+        labelCategoryNew.textContent = "Category";
+        divInputZone2.insertBefore(labelCategoryNew, selectCategoryNew);
+        
+        GformNew.appendChild(divInputZone2);
 
         // Fetch categories from the API
         fetch('http://localhost:5678/api/categories', {
@@ -92,10 +123,17 @@ function afficherPopupNew() {
                 throw error;
             });
 
+            let submitBtnNewWrapper = document.createElement("div");
+            submitBtnNewWrapper.classList.add("sub_Wrapper");
+
         let submitBtnNew = document.createElement("input");
         submitBtnNew.setAttribute("type", "submit");
         submitBtnNew.value = "Valider";
+        submitBtnNew.classList.add("submit-btn-new");
         GformNew.appendChild(submitBtnNew);
+
+        submitBtnNewWrapper.appendChild(submitBtnNew);
+                    GformNew.appendChild(submitBtnNewWrapper);
 
         popupBackgroundNew.addEventListener("click", (event) => {
             if (event.target === popupBackgroundNew) {
